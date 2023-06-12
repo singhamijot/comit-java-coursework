f7d5de52fd0ba143b0d710c20ab90ed5ffa6d366
unknown <singhamijot@gmail.com>
2023-06-12 15:39:30 -0600

    Ex3 Upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex3.java
@@ -0,0 +1,13 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		
+		
+		float num1= 5.3f;
+		System.out.println(num1);
+		
+		
+	}
+
+}
