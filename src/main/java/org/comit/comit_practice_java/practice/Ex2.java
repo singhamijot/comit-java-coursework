af10684b8fd11b0602662585d98138f1a9b611ce
unknown <singhamijot@gmail.com>
2023-06-12 15:32:51 -0600

    Ex2 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex2.java
@@ -0,0 +1,13 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1= 273;
+		int num2=597;
+		
+		System.out.println("" + num1 + " "+ num2 );
+	}
+
+}
