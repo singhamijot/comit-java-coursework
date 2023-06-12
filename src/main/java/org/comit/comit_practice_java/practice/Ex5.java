ce76678ded73fbdab97b8faed0c391e103c2f2ea
unknown <singhamijot@gmail.com>
2023-06-12 15:43:11 -0600

    Ex5 Upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex5.java
@@ -0,0 +1,14 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 1234;
+		int n2 = 532;
+		
+		System.out.println(n1-n2);
+		
+	}
+
+}
