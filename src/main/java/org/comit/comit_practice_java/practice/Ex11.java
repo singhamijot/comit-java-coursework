6d9468e02547b95e87be2e84bc893df8c62ea7c1
unknown <singhamijot@gmail.com>
2023-06-12 16:07:14 -0600

    Ex11 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex11.java
@@ -0,0 +1,12 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'A';
+		
+		System.out.println((int)a);
+	}
+
+}
