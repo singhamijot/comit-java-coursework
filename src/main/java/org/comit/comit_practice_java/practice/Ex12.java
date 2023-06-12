6b0b1ffcfe58f597f542e5015fd60e2ad72fedd1
unknown <singhamijot@gmail.com>
2023-06-12 16:12:05 -0600

    Ex12 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex12.java
@@ -0,0 +1,14 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 7.5;
+		double area = 3.14 * radius * radius;
+		double perimeter = 3.14 * 2 * radius ;
+		
+		System.out.println("Area : "+area +" and Perimeter : "+perimeter);
+	}
+
+}
