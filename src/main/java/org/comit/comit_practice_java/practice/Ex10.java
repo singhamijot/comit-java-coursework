bc9c91b33f723c017d96b4575b4d5420214a0397
unknown <singhamijot@gmail.com>
2023-06-12 16:05:02 -0600

    Ex10 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex10.java
@@ -0,0 +1,14 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 25;
+		int sum = a % 10;
+		a/=10;
+		sum+=a;
+		System.out.println(sum);
+	}
+
+}
