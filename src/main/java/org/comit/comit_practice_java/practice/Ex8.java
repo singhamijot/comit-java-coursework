475b1f546ed20f10ffc445a7bbb1d478eecd3932
unknown <singhamijot@gmail.com>
2023-06-12 15:52:19 -0600

    Ex8 Upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex8.java
@@ -0,0 +1,15 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println((101+0)/(double)3);
+		System.out.println((double)3.0e-6 * 10000000.1);
+		System.out.println(true&&true);
+		System.out.println(false&&true);
+		System.out.println((false && false) || (true && true));
+		System.out.println((false || false) && (true && true));
+	}
+
+}
