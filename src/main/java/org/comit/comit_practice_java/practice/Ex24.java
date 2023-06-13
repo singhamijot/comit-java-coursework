5806f14620269cbfe412b45be34b959b3072027e
unknown <singhamijot@gmail.com>
2023-06-13 15:53:59 -0600

    Ex24 Upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex24.java
@@ -0,0 +1,14 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = new String("Hello");
+		for(int i=0;i<200;i++)
+		{
+			System.out.println(""+i+" "+str1);
+		}
+	}
+
+}
