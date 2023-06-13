11e6e34fe0cf6b1d746e8cb8c81c628013731d70
unknown <singhamijot@gmail.com>
2023-06-13 16:19:32 -0600

    Ex29 Adding

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex29.java
@@ -0,0 +1,17 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =5;
+		double b = 10.5;
+		char c ='a';
+		
+		System.out.println(" n: "+ n  + " b : "+b + " c : "+c);
+		System.out.printf("Sum of n + b : %f \n", (n+b));
+		System.out.printf("Sum of b - n  : %f", (b - n));
+		
+	}
+
+}
