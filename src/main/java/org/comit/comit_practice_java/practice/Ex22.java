7b33ae89eb75ec7679f704e098bef60f6d7b68f9
unknown <singhamijot@gmail.com>
2023-06-13 15:49:26 -0600

    Ex22 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex22.java
@@ -0,0 +1,13 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i<=15;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
