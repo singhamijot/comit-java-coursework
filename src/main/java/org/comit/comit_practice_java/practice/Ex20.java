8e5897880142bd2c2bf1f133a8dab3005d76a921
unknown <singhamijot@gmail.com>
2023-06-13 15:46:45 -0600

    Ex20

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex20.java
@@ -0,0 +1,12 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10000;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
