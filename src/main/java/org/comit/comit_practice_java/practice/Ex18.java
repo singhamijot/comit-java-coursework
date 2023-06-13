9702f88559d05e12d97c7dd01f76e56d4df3e792
unknown <singhamijot@gmail.com>
2023-06-13 15:45:07 -0600

    Adding Exercises

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex18.java
@@ -0,0 +1,12 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=3;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
