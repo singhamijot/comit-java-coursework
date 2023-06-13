94a552137ace47ff6360c9c2c2764ee6f9be2908
unknown <singhamijot@gmail.com>
2023-06-13 16:21:52 -0600

    Adding Ex30

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex30.java
@@ -0,0 +1,17 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		int[] array = {7, 3, 6, 2, 8, 1, 5, 4, 9, 10};
+		for (int i : array)
+		{
+			sum+=i;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
