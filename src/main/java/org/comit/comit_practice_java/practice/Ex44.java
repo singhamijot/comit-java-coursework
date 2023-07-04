cbcd98edb650b8e4c53399dc5b5ce2683e3a7705
unknown <singhamijot@gmail.com>
2023-07-04 13:22:48 -0600

    Ex44 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex44.java
@@ -0,0 +1,13 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex44 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "educated";
+		String com = str.substring(0,2);
+		boolean bool = str.endsWith(com);
+		System.out.println(bool);
+	}
+
+}
