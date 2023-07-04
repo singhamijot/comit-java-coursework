1f32b4aff6fcc5f5f132fd3c2f612e4cda784189
unknown <singhamijot@gmail.com>
2023-07-03 19:35:28 -0600

    Ex38 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex38.java
@@ -0,0 +1,12 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex38 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I love coding in Java";
+		int i = str.charAt(8);
+		System.out.println(i);
+	}
+
+}
