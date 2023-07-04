7c66808ae81f6af411c9f2971183b2c807d4e7f7
unknown <singhamijot@gmail.com>
2023-07-03 19:41:12 -0600

    Ex40 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex40.java
@@ -0,0 +1,12 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I love coding in Java";
+		boolean bool = str.endsWith("ava");
+		System.out.println(bool);
+	}
+
+}
