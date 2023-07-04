bd7c4b6e18fc0484eecf139620ab39a4da09fefe
unknown <singhamijot@gmail.com>
2023-07-03 19:30:09 -0600

    uploading ex37

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex37.java
@@ -0,0 +1,12 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I love coding in Java";
+		System.out.println(str.charAt(7));
+		
+	}
+
+}
