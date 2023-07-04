941190a8b9561fcc1d90e13912a921a6100bdad7
unknown <singhamijot@gmail.com>
2023-07-03 19:43:48 -0600

    Ex41 upload

---
+++ src/main/java/org/comit/comit_practice_java/practice/Ex41.java
@@ -0,0 +1,13 @@
+package org.comit.comit_practice_java.practice;
+
+public class Ex41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "I love coding in Java";
+		String str2 = "I Love Coding In Java";
+		boolean bool = str1.matches(str2);
+		System.out.println(bool);
+	}
+
+}
